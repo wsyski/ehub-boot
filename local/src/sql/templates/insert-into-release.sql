43a6252857c3f2d4f27043cc3579a5597956150d
wos <wsyski@gmail.com>
2013-03-18 22:09:17 +0100

    ARENA-9948 - Create an PL/SQL script that improves the generated eHUB database schema

--- local/src/sql/templates/insert-into-release.sql
+++ local/src/sql/templates/insert-into-release.sql
@@ -1,4 +1,10 @@
-insert into release(version,create_datetime,modify_datetime) values('${project.version}',systimestamp,systimestamp);
+merge into release r
+using dual on (r.version= '${project.version}')
+when not matched then
+insert(r.version,r.create_datetime,r.modify_datetime) values('${project.version}',systimestamp,systimestamp)
+when matched then
+update set r.modify_datetime = systimestamp;
 commit;
+
 
 
