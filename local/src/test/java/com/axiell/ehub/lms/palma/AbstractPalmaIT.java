19d0902fad32f61fd905833a01cf2df21115ceec
wos <wsyski@gmail.com>
2013-10-01 22:24:29 +0200

    ARENA-12171 - Create PALMA integration tests for each production version of Arena

--- local/src/test/java/com/axiell/ehub/lms/palma/AbstractPalmaIT.java
+++ local/src/test/java/com/axiell/ehub/lms/palma/AbstractPalmaIT.java
@@ -1,5 +1,86 @@
 package com.axiell.ehub.lms.palma;
 
-abstract class AbstractPalmaIT {
+import com.axiell.ehub.DevelopmentData;
+import com.axiell.ehub.consumer.EhubConsumer;
+import com.axiell.ehub.loan.LmsLoan;
+import com.axiell.ehub.loan.PendingLoan;
+import com.axiell.ehub.provider.ContentProviderName;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.util.ReflectionTestUtils;
 
+import javax.xml.ws.Endpoint;
+import java.util.Date;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("classpath:/com/axiell/ehub/common-context.xml")
+public abstract class AbstractPalmaIT<T> {
+    private Endpoint endpoint;
+
+    @Autowired
+    private PalmaDataAccessor palmaDataAccessor;
+
+    private EhubConsumer ehubConsumer;
+    private PendingLoan pendingLoan;
+    private CheckoutTestAnalysis preCheckoutAnalysis;
+    private LmsLoan lmsLoan;
+
+    @Before
+    public void setUp() throws Exception {
+        ehubConsumer = DevelopmentData.createEhubConsumer();
+        pendingLoan = new PendingLoan(DevelopmentData.LMS_RECORD_ID, ContentProviderName.ELIB.name(), DevelopmentData.ELIB_RECORD_0_ID,
+                DevelopmentData.ELIB_FORMAT_0_ID);
+        String palmaUrl = ehubConsumer.getProperties().get(EhubConsumer.EhubConsumerPropertyKey.ARENA_PALMA_URL);
+        ReflectionTestUtils.setField(ehubConsumer, "id", 1L);
+        endpoint = Endpoint.publish(palmaUrl + "/loans", getPalmaLoansServiceInstance());
+    }
+
+    @After
+    public void tearDown() {
+        endpoint.stop();
+    }
+
+    @Test
+    public void testCheckOutTest() {
+        whenCheckOutTestExecuted();
+        thenActiveLoanReturned();
+    }
+
+    @Test
+    public void testCheckOut() {
+        whenCheckOutExecuted();
+        thenLmsLoanIsReturned();
+    }
+
+    private void thenLmsLoanIsReturned() {
+        assertNotNull(lmsLoan);
+        assertEquals(DevelopmentData.LMS_LOAN_ID_1, lmsLoan.getId());
+    }
+
+    private void thenActiveLoanReturned() {
+        assertNotNull(preCheckoutAnalysis);
+        assertEquals(CheckoutTestAnalysis.Result.ACTIVE_LOAN, preCheckoutAnalysis.getResult());
+        assertEquals(DevelopmentData.LMS_LOAN_ID_1, preCheckoutAnalysis.getLmsLoanId());
+    }
+
+    private void whenCheckOutTestExecuted() {
+        preCheckoutAnalysis =
+                palmaDataAccessor.checkoutTest(ehubConsumer, pendingLoan, DevelopmentData.ELIB_LIBRARY_CARD, DevelopmentData.ELIB_LIBRARY_CARD_PIN);
+    }
+
+    private void whenCheckOutExecuted() {
+        Date expirationDate = new Date();
+        lmsLoan = palmaDataAccessor.checkout(ehubConsumer, pendingLoan, expirationDate, DevelopmentData.ELIB_LIBRARY_CARD,
+                DevelopmentData.ELIB_LIBRARY_CARD_PIN);
+    }
+
+    protected abstract T getPalmaLoansServiceInstance();
 }
---
+++ local/src/test/java/com/axiell/ehub/lms/palma/PalmaLoansService_265.java
@@ -0,0 +1,58 @@
+package com.axiell.ehub.lms.palma;
+
+import com.axiell.arena.services.palma_265.loans.*;
+import com.axiell.arena.services.palma_265.patron.checkoutrequest.CheckOutRequest;
+import com.axiell.arena.services.palma_265.patron.checkouttestrequest.CheckOutTestRequest;
+import com.axiell.ehub.DevelopmentData;
+import junit.framework.Assert;
+
+import javax.jws.WebService;
+
+
+@WebService(serviceName = "LoansPalmaService", portName = "loans", targetNamespace = "http://loans.palma.services.arena.axiell.com/",
+        wsdlLocation = "com/axiell/arena/palma_265/loans.wsdl", endpointInterface = "com.axiell.arena.services.palma_265.loans.Loans")
+public class PalmaLoansService_265 extends AbstractPalmaLoansService implements Loans {
+    private static final String CONTEXT_PATH = "com.axiell.arena.services.palma_265.loans";
+
+    public CheckOutTestResponse checkOutTest(final CheckOutTest parameters) {
+        verifyCheckOutTest(parameters);
+        return getFileResponseUnmarshaller().unmarshalFromFile("com/axiell/arena/palma/CheckOutTestResponse.xml");
+    }
+
+    public CheckOutResponse checkOut(final CheckOut parameters) {
+        verifyCheckOut(parameters);
+        return getFileResponseUnmarshaller().unmarshalFromFile("com/axiell/arena/palma/CheckOutResponse.xml");
+    }
+
+    @Override
+    public RenewLoansResponse renewLoans(final RenewLoans parameters) {
+        return null;
+    }
+
+    @Override
+    public GetLoansResponse getLoans(final GetLoans parameters) {
+        return null;
+    }
+
+    protected String getContextPath() {
+        return CONTEXT_PATH;
+    }
+
+    protected void verifyCheckOutTest(final CheckOutTest checkOutTest) {
+        CheckOutTestRequest checkOutTestRequest = checkOutTest.getCheckOutTestRequest();
+        Assert.assertNotNull(checkOutTestRequest);
+        Assert.assertEquals(checkOutTestRequest.getArenaMember(), DevelopmentData.ARENA_AGENCY_M_IDENTIFIER);
+        Assert.assertEquals(checkOutTestRequest.getRecordId(), DevelopmentData.LMS_RECORD_ID);
+        Assert.assertEquals(checkOutTestRequest.getUser(), DevelopmentData.ELIB_LIBRARY_CARD);
+        Assert.assertEquals(checkOutTestRequest.getPassword(), DevelopmentData.ELIB_LIBRARY_CARD_PIN);
+    }
+
+    protected void verifyCheckOut(final CheckOut checkOut) {
+        CheckOutRequest checkOutRequest = checkOut.getCheckOutRequest();
+        Assert.assertNotNull(checkOutRequest);
+        Assert.assertEquals(checkOutRequest.getArenaMember(), DevelopmentData.ARENA_AGENCY_M_IDENTIFIER);
+        Assert.assertEquals(checkOutRequest.getRecordId(), DevelopmentData.LMS_RECORD_ID);
+        Assert.assertEquals(checkOutRequest.getUser(), DevelopmentData.ELIB_LIBRARY_CARD);
+        Assert.assertEquals(checkOutRequest.getPassword(), DevelopmentData.ELIB_LIBRARY_CARD_PIN);
+    }
+}
