53e1e84dfab6f2bcdc34d85f8e921e053dcef302
psk <psk@axiell.com>
2013-10-16 14:31:46 +0200

    Removed unused imports

--- local/src/main/java/com/axiell/ehub/loan/ILoanBusinessController.java
+++ local/src/main/java/com/axiell/ehub/loan/ILoanBusinessController.java
@@ -3,43 +3,41 @@
  */
 package com.axiell.ehub.loan;
 
-import com.axiell.ehub.provider.ContentProvider;
 import com.axiell.ehub.security.AuthInfo;
-import com.axiell.ehub.security.UnauthorizedException;
 
 /**
- * Defines all business methods related to {@link PendingLoan}s and {@link ReadyLoan}s.
+ * Defines all business methods related to PendingLoan}s and ReadyLoans.
  * 
  * <p>This interface should only be used by the resources of the eHUB REST interface or by other business controllers.</p>
  */
 public interface ILoanBusinessController {
 
     /**
-     * Creates a loan both in the LMS and at the {@link ContentProvider}.
+     * Creates a loan both in the LMS and at the ContentProvider.
      * 
-     * @param authInfo an {@link AuthInfo} containing an eHUB consumer ID, library card and pin
-     * @param pendingLoan a {@link PendingLoan}
-     * @return a {@link ReadyLoan}
-     * @throws UnauthorizedException,ForbiddenException
+     * @param authInfo an AuthInfo containing an eHUB consumer ID, library card and pin
+     * @param pendingLoan a PendingLoan
+     * @return a ReadyLoan
+     * @throws UnauthorizedException, ForbiddenException
      */
     ReadyLoan createLoan(AuthInfo authInfo, PendingLoan pendingLoan);
 
     /**
-     * Gets a {@link ReadyLoan} for the provided {@link AuthInfo} and the globally unique ID of the {@link ReadyLoan}.
+     * Gets a ReadyLoan for the provided AuthInfo and the globally unique ID of the ReadyLoan.
      * 
-     * @param authInfo an {@link AuthInfo} containing an eHUB consumer ID, library card and pin
-     * @param readyLoanId the globally unique ID of the {@link ReadyLoan}
-     * @return a {@link ReadyLoan}
+     * @param authInfo an AuthInfo containing an eHUB consumer ID, library card and pin
+     * @param readyLoanId the globally unique ID of the ReadyLoan
+     * @return a ReadyLoan
      * @throws UnauthorizedException
      */
     ReadyLoan getReadyLoan(AuthInfo authInfo, Long readyLoanId);
 
     /**
-     * Gets a {@link ReadyLoan} for the provided {@link AuthInfo} and LMS loan ID.
+     * Gets a ReadyLoan for the provided AuthInfo and LMS loan ID.
      * 
-     * @param authInfo an {@link AuthInfo} containing an eHUB consumer ID, library card and pin
+     * @param authInfo an AuthInfo containing an eHUB consumer ID, library card and pin
      * @param lmsLoanId the ID of the loan in the LMS
-     * @return a {@link ReadyLoan}
+     * @return a ReadyLoan
      * @throws UnauthorizedException
      */
     ReadyLoan getReadyLoan(AuthInfo authInfo, String lmsLoanId);
