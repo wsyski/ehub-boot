820e957dd1ebf78428b966d0d51a3f2315bbdcee
psk <psk@axiell.com>
2013-10-18 17:00:31 +0200

    Refactored admin interface

--- local/src/main/java/com/axiell/ehub/user/LoginPanel.java
+++ local/src/main/java/com/axiell/ehub/user/LoginPanel.java
@@ -3,38 +3,15 @@
  */
 package com.axiell.ehub.user;
 
-import org.apache.wicket.markup.html.form.Button;
 import org.apache.wicket.markup.html.form.PasswordTextField;
 import org.apache.wicket.markup.html.form.RequiredTextField;
 import org.apache.wicket.markup.html.form.StatelessForm;
 import org.apache.wicket.markup.html.panel.FeedbackPanel;
 import org.apache.wicket.markup.html.panel.Panel;
 import org.apache.wicket.model.CompoundPropertyModel;
-import org.apache.wicket.model.Model;
-import org.apache.wicket.model.StringResourceModel;
-import org.apache.wicket.spring.injection.annot.SpringBean;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
-import com.axiell.ehub.EhubAdminPage;
-import com.axiell.ehub.EhubAdminSession;
-
-/**
- * A {@link Panel} that provides the possibility for the end-user to login to the eHUB Administration interface. If the
- * end-user is successfully logged in she is sent to the {@link EhubAdminPage}.
- */
 public final class LoginPanel extends Panel {
-    private static final long serialVersionUID = -8387035246210184104L;
-    private static final Logger LOGGER = LoggerFactory.getLogger(LoginPanel.class);
 
-    @SpringBean(name = "userAdminController")
-    private IUserAdminController userAdminController;
-
-    /**
-     * Constructs a new {@link LoginPanel}.
-     * 
-     * @param panelId the ID of this {@link Panel}
-     */
     public LoginPanel(String panelId) {
         super(panelId);
 
@@ -51,35 +28,8 @@ public final class LoginPanel extends Panel {
         final PasswordTextField passwordField = new PasswordTextField("clearPassword");
         loginForm.add(passwordField);
 
-        final Button submitButton = new Button("submit") {
-            private static final long serialVersionUID = 8650216690439979415L;
-
-            /**
-             * @see org.apache.wicket.markup.html.form.Button#onSubmit()
-             */
-            @Override
-            public void onSubmit() {
-                final LoginStatus status = userAdminController.login(user);
-
-                switch (status) {
-                    case SUCCESS:
-                        final EhubAdminSession session = (EhubAdminSession) getSession();
-                        session.setAdminUser(user);
-                        final EhubAdminPage ehubAdminPage = new EhubAdminPage(user);
-                        setResponsePage(ehubAdminPage);
-                        break;
-                    default:
-                        if (LOGGER.isDebugEnabled()) {
-                            LOGGER.debug("Login status for user '" + user.getName() + "': '" + status + "'");
-                        }
-                        final StringResourceModel resourceModel = new StringResourceModel("msgInvalidCredentials", LoginPanel.this, new Model<>());
-                        final String errorMessage = resourceModel.getString();
-                        error(errorMessage);
-                        break;
-                }
-            }
-        };
-        loginForm.add(submitButton);
+        final LoginButton loginButton = new LoginButton("submit", user);
+        loginForm.add(loginButton);
 
         add(loginForm);
     }
--- local/src/main/java/com/axiell/ehub/user/LogoutPanel.java
+++ local/src/main/java/com/axiell/ehub/user/LogoutPanel.java
@@ -6,35 +6,15 @@ package com.axiell.ehub.user;
 import org.apache.wicket.markup.html.link.Link;
 import org.apache.wicket.markup.html.panel.Panel;
 
-
-/**
- * A {@link Panel} that provides the possibility to logout from the eHUB Administration interface. If the end-user
- * decides to logout she is sent to the home page of the EhubAdminApplication.
- */
 public final class LogoutPanel extends Panel {
-    private static final long serialVersionUID = -4761711573336380963L;
 
-    /**
-     * Constructs a new {@link LogoutPanel}.
-     * 
-     * @param id the ID of this {@link Panel}
-     */
     public LogoutPanel(final String id) {
         super(id);
+        addLogoutLink();
+    }
 
-        final Link<Void> logoutLink = new Link<Void>("logout") {
-            private static final long serialVersionUID = 8215228269723379869L;
-
-            /**
-             * @see org.apache.wicket.markup.html.link.Link#onClick()
-             */
-            @Override
-            public void onClick() {
-                getSession().invalidate();
-                setResponsePage(getApplication().getHomePage());
-                getRequestCycle().setRedirect(true);
-            }
-        };
+    private void addLogoutLink() {
+	final Link<Void> logoutLink = new LogoutLink("logout");
         add(logoutLink);
     }
 
