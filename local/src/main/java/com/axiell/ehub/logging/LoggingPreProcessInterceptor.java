a3c714a83a62971f42336c01ad87e31b6160334f
psk <psk@axiell.com>
2013-10-22 13:01:32 +0200

    Refactored admin interface

--- local/src/main/java/com/axiell/ehub/logging/LoggingPreProcessInterceptor.java
+++ local/src/main/java/com/axiell/ehub/logging/LoggingPreProcessInterceptor.java
@@ -1,5 +1,7 @@
 package com.axiell.ehub.logging;
 
+import javax.ws.rs.ext.Provider;
+
 import org.apache.commons.lang3.time.StopWatch;
 import org.jboss.resteasy.annotations.interception.ServerInterceptor;
 import org.jboss.resteasy.core.ResourceMethod;
@@ -10,10 +12,6 @@ import org.jboss.resteasy.spi.interception.PreProcessInterceptor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.ws.rs.core.Context;
-import javax.ws.rs.ext.Provider;
-
 /**
  * Logging post process interceptor
  */
@@ -21,9 +19,6 @@ import javax.ws.rs.ext.Provider;
 @ServerInterceptor
 public final class LoggingPreProcessInterceptor implements PreProcessInterceptor {
     private static final Logger LOGGER = LoggerFactory.getLogger(LoggingPreProcessInterceptor.class);
-
-    @Context
-    private HttpServletRequest servletRequest;
 
     @Override
     public ServerResponse preProcess(HttpRequest httpRequest, ResourceMethod method) throws Failure {
