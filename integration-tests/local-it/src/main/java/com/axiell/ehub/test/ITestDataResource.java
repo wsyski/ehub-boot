f2aa7f005fd893c30bc6d8f242834a3a370fec39
Pär Sarbäck <psk@axiell.com>
2014-06-02 12:25:16 +0200

    ARENA-13514

--- integration-tests/local-it/src/main/java/com/axiell/ehub/test/ITestDataResource.java
+++ integration-tests/local-it/src/main/java/com/axiell/ehub/test/ITestDataResource.java
@@ -3,12 +3,15 @@ package com.axiell.ehub.test;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 @Path("v1/test-data")
 public interface ITestDataResource {
 
     @POST
+    @Produces(MediaType.APPLICATION_XML)
     TestData init();
 
     @DELETE
