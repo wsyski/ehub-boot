c2234cc707ce856b1dfd25b85b4e2d02c08a2ffe
psk <par.sarback@gmail.com>
2014-06-05 09:06:06 +0200

    ARENA-13514

---
+++ local/src/test/java/com/axiell/ehub/provider/elib/library3/Elib3CommandChainFactoryTest.java
@@ -0,0 +1,34 @@
+package com.axiell.ehub.provider.elib.library3;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.test.util.AssertionErrors;
+
+import static org.junit.Assert.assertNotNull;
+
+public class Elib3CommandChainFactoryTest {
+    private Elib3CommandChainFactory underTest;
+
+    @Before
+    public void setUpUnderTest() {
+        underTest = new Elib3CommandChainFactory();
+    }
+
+    @Test
+    public void createGetFormatsCommandChain() {
+        GetFormatsCommandChain commandChain = underTest.createGetFormatsCommandChain();
+        assertNotNull(commandChain);
+    }
+
+    @Test
+    public void createCreateLoanCommandChain() {
+        CreateLoanCommandChain commandChain = underTest.createCreateLoanCommandChain();
+        assertNotNull(commandChain);
+    }
+
+    @Test
+    public void createGetContentCommandChain() {
+        GetContentCommandChain commandChain = underTest.createGetContentCommandChain();
+        assertNotNull(commandChain);
+    }
+}
