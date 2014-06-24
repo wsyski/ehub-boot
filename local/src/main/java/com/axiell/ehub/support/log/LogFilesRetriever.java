72d90fe39605969ae86aded0e01698191d8f52ef
psk <par.sarback@gmail.com>
2014-06-24 11:16:44 +0200

    ARENA-13514

--- local/src/main/java/com/axiell/ehub/support/log/LogFilesRetriever.java
+++ local/src/main/java/com/axiell/ehub/support/log/LogFilesRetriever.java
@@ -3,6 +3,8 @@ package com.axiell.ehub.support.log;
 import com.google.common.collect.Lists;
 
 import java.io.File;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 class LogFilesRetriever {
@@ -14,7 +16,14 @@ class LogFilesRetriever {
         final File logFilesFolder = new File(System.getProperty("catalina.base") + File.separator + "logs");
         if (logFilesFolder.exists()) {
             final File[] files = logFilesFolder.listFiles();
-            return Lists.newArrayList(files);
+            final List<File> fileList = Lists.newArrayList(files);
+            Collections.sort(fileList, new Comparator<File>() {
+                @Override
+                public int compare(File o1, File o2) {
+                    return o1.getName().compareTo(o2.getName());
+                }
+            });
+            return fileList;
         }
         return Lists.newArrayList();
     }
