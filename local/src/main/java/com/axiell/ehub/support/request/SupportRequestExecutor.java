593f2f1d7de8bcfe3ac6b5bf5cdbb73b53ddcf81
psk <par.sarback@gmail.com>
2014-06-24 08:43:20 +0200

    Sonar fixes

--- local/src/main/java/com/axiell/ehub/support/request/SupportRequestExecutor.java
+++ local/src/main/java/com/axiell/ehub/support/request/SupportRequestExecutor.java
@@ -7,6 +7,7 @@ import com.axiell.ehub.provider.IContentProvidersResource;
 import com.axiell.ehub.provider.record.IRecordsResource;
 import com.axiell.ehub.provider.record.format.Formats;
 import com.axiell.ehub.security.AuthInfo;
+import com.axiell.ehub.util.EhubUrlCodec;
 import com.axiell.ehub.util.XjcSupport;
 import org.jboss.resteasy.client.*;
 import org.jboss.resteasy.client.core.executors.ApacheHttpClient4Executor;
@@ -29,7 +30,8 @@ class SupportRequestExecutor {
             supportRequest.setAuthInfo(authInfo);
             final ClientExecutor clientExecutor = new SupportClientExecutor(supportRequest);
             final IContentProvidersResource contentProvidersResource = ProxyFactory.create(IContentProvidersResource.class, baseUri, clientExecutor);
-            final IRecordsResource recordsResource = contentProvidersResource.getRecords(contentProviderName);
+//            final IRecordsResource recordsResource = contentProvidersResource.getRecords(contentProviderName);
+            final IRecordsResource recordsResource = contentProvidersResource.getRecords(EhubUrlCodec.encode(contentProviderName));
             final Formats formats = recordsResource.getFormats(authInfo, contentProviderRecordId, language);
             return makeSupportResponse(supportRequest, STATUS_OK, formats);
         } catch (ClientResponseFailure crf) {
