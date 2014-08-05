5ff9aeeb5c06ba3e106142805258daff28f4219f
psk <par.sarback@gmail.com>
2014-08-05 09:06:43 +0200

    Added support for Liquibase

--- local/src/main/java/com/axiell/ehub/version/WarFileManifest.java
+++ local/src/main/java/com/axiell/ehub/version/WarFileManifest.java
@@ -1,71 +1,69 @@
 package com.axiell.ehub.version;
 
-import java.io.IOException;
-
-import javax.servlet.ServletContext;
-
+import com.jcabi.manifests.Manifests;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.jcabi.manifests.Manifests;
+import javax.servlet.ServletContext;
+import java.io.IOException;
 
 public class WarFileManifest {
     private final String implementationVersion;
     private final String buildTime;
 
     private WarFileManifest(final String implementationVersion, final String buildTime) {
-	this.implementationVersion = implementationVersion;
-	this.buildTime = buildTime;
+        this.implementationVersion = implementationVersion;
+        this.buildTime = buildTime;
     }
 
     String getImplementationVersion() {
-	return implementationVersion;
+        return implementationVersion;
     }
 
     String getBuildTime() {
-	return buildTime;
+        return buildTime;
     }
 
     static WarFileManifest read(final ServletContext servletContext) throws ManifestException {
-	return new Builder(servletContext).implementationVersion().buildTime().build();
+        return new Builder(servletContext).implementationVersion().buildTime().build();
     }
 
     private static class Builder {
-	private static final Logger LOGGER = LoggerFactory.getLogger(Builder.class);
-	private static final String BUILD_TIME = "Build-Time";
-	private static final String IMPLEMENTATION_VERSION = "Implementation-Version";
-	private String implementationVersion;
-	private String buildTime;
+        private static final Logger LOGGER = LoggerFactory.getLogger(Builder.class);
+        private static final String BUILD_TIME = "Build-Time";
+        private static final String IMPLEMENTATION_VERSION = "Implementation-Version";
+        private String implementationVersion;
+        private String buildTime;
 
-	private Builder(final ServletContext servletContext) throws ManifestException {
-	    try {
-		Manifests.append(servletContext);
-	    } catch (IOException e) {
-		throw new ManifestException(e);
-	    }
-	}
+        private Builder(final ServletContext servletContext) throws ManifestException {
+            try {
+                Manifests.append(servletContext);
+            } catch (IOException e) {
+                throw new ManifestException(e);
+            }
+        }
 
-	Builder implementationVersion() {
-	    this.implementationVersion = read(IMPLEMENTATION_VERSION);
-	    return this;
-	}
+        Builder implementationVersion() {
+            this.implementationVersion = read(IMPLEMENTATION_VERSION);
+            return this;
+        }
 
-	Builder buildTime() {
-	    this.buildTime = read(BUILD_TIME);
-	    return this;
-	}
-	
-	private String read(String name) {
-	    try {
-		return Manifests.read(name);
-	    } catch (IllegalArgumentException e) {
-		LOGGER.warn("Could not find attribute with name '" + name + "' in Manifest");
-		return null;
-	    }
-	}
+        Builder buildTime() {
+            this.buildTime = read(BUILD_TIME);
+            return this;
+        }
 
-	WarFileManifest build() {
-	    return new WarFileManifest(implementationVersion, buildTime);
-	}
+        private String read(String name) {
+            if (Manifests.exists(name))
+                return Manifests.read(name);
+            else {
+                LOGGER.warn("Could not find attribute with name '" + name + "' in Manifest");
+                return null;
+            }
+        }
+
+        WarFileManifest build() {
+            return new WarFileManifest(implementationVersion, buildTime);
+        }
     }
 }
