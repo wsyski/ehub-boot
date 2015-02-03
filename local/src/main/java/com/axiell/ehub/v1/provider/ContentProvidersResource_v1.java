c8736a32e8ad04089b40f8843fb21fc6537bf82d
psk <par.sarback@gmail.com>
2015-02-03 14:22:01 +0100

    ARENA-15531

--- local/src/main/java/com/axiell/ehub/v1/provider/ContentProvidersResource_v1.java
+++ local/src/main/java/com/axiell/ehub/v1/provider/ContentProvidersResource_v1.java
@@ -8,9 +8,6 @@ import com.axiell.ehub.v1.provider.record.IRecordsResource_v1;
 import com.axiell.ehub.v1.provider.record.RecordsResource_v1;
 import org.springframework.beans.factory.annotation.Required;
 
-/**
- * Default implementation of the {@link com.axiell.ehub.provider.IContentProvidersResource}.
- */
 public final class ContentProvidersResource_v1 implements IContentProvidersResource_v1 {
     private IFormatBusinessController formatBusinessController;
 
