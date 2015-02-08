626137c3b206fee2d28b72d0e4ce34ca683b94ed
wsyski <wos@axiell.com>
2015-02-08 20:16:09 +0100

    Arena-15531

---
+++ integration-tests/tests/remote-0.7/src/test/java/com/axiell/ehub/it07/RemoteFormatITFixture.java
@@ -0,0 +1,59 @@
+package com.axiell.ehub.it07;
+
+import com.axiell.ehub.EhubException;
+import com.axiell.ehub.provider.record.format.Format;
+import com.axiell.ehub.provider.record.format.Formats;
+import com.axiell.ehub.security.AuthInfo;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Locale;
+import java.util.Set;
+
+import static com.github.tomakehurst.wiremock.client.WireMock.*;
+
+public abstract class RemoteFormatITFixture extends RemoteITFixture {
+    protected static final String LANGUAGE = Locale.ENGLISH.getLanguage();
+    protected Formats actualFormats;
+
+    @Override
+    protected void initAuthInfo() throws EhubException {
+        authInfo = new AuthInfo.Builder(testData.getEhubConsumerId(), testData.getEhubConsumerSecretKey()).libraryCard(testData.getLibraryCard()).pin(testData.getPin()).build();
+    }
+
+    @Test
+    public final void getFormats() throws EhubException {
+        givenGetProductResponse();
+        whenGetFormats();
+        thenActualFormatsContainsExpectedComponents();
+        thenCustomFormatsValidation();
+    }
+
+    private void givenGetProductResponse() {
+        stubFor(post(urlEqualTo("/webservices/GetProduct.asmx/GetProduct")).willReturn(aResponse().withBodyFile("GetProductResponse.xml").withHeader(
+                "Content-Type", "application/xml").withStatus(200)));
+    }
+
+    protected abstract void whenGetFormats() throws EhubException;
+
+    private void thenActualFormatsContainsExpectedComponents() {
+        Assert.assertNotNull(actualFormats);
+        Set<Format> formatSet = actualFormats.getFormats();
+        Assert.assertNotNull(formatSet);
+        Assert.assertFalse(formatSet.isEmpty());
+
+        for (Format format : formatSet) {
+            thenFormatContainsExpectedComponents(format);
+        }
+    }
+
+    private void thenFormatContainsExpectedComponents(Format format) {
+        String id = format.getId();
+        Assert.assertNotNull(id);
+        String name = format.getName();
+        Assert.assertNotNull(name);
+    }
+
+    protected void thenCustomFormatsValidation() {
+    }
+}
