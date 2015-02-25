e1383a843a330e2249b8af09f1025070d4bdb07c
wsyski <wos@axiell.com>
2015-02-25 14:29:44 +0100

    Arena-15531

---
+++ common/src/main/java/com/axiell/ehub/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * High-level components used by the Axiell eHUB and its clients 
+ */
+@XmlSchema(xmlns = {@XmlNs(namespaceURI = "http://ehub.axiell.com", prefix = "ehub")}, namespace = "http://ehub.axiell.com", elementFormDefault = XmlNsForm.UNSET, attributeFormDefault = XmlNsForm.UNSET)
+package com.axiell.ehub;
+
+import javax.xml.bind.annotation.XmlNs;
+import javax.xml.bind.annotation.XmlNsForm;
+import javax.xml.bind.annotation.XmlSchema;
+
