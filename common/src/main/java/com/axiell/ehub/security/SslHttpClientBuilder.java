a96cc3fcf078d9768fc0dea111dd383f0e8a5ddf
wsyski <wos@axiell.com>
2015-04-01 19:53:16 +0200

    ARENA-15797 - FAULT - ebook - Det finns inget tillgängligt format för denna e-bok

--- common/src/main/java/com/axiell/ehub/security/SslHttpClientBuilder.java
+++ common/src/main/java/com/axiell/ehub/security/SslHttpClientBuilder.java
@@ -2,9 +2,10 @@ package com.axiell.ehub.security;
 
 import org.apache.http.conn.ClientConnectionManager;
 import org.apache.http.conn.scheme.Scheme;
+import org.apache.http.conn.scheme.SchemeRegistry;
 import org.apache.http.conn.ssl.SSLSocketFactory;
 import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.impl.conn.tsccm.ThreadSafeClientConnManager;
+import org.apache.http.impl.conn.PoolingClientConnectionManager;
 import org.apache.http.params.BasicHttpParams;
 import org.apache.http.params.HttpParams;
 
@@ -35,18 +36,18 @@ public final class SslHttpClientBuilder {
     private static ClientConnectionManager makeClientConnectionManager(int port) {
         final SSLSocketFactory socketFactory = makeSocketFactory();
         final Scheme httpsScheme = new Scheme(SCHEME_NAME, port, socketFactory);
-        final ClientConnectionManager connectionManager = new ThreadSafeClientConnManager();
+        final ClientConnectionManager connectionManager = new PoolingClientConnectionManager();
         connectionManager.getSchemeRegistry().register(httpsScheme);
         return connectionManager;
     }
 
     private static SSLSocketFactory makeSocketFactory() {
-        final X509TrustManager trustManager = new UnsafeX509TrustManager();
-        final SSLContext sslContext = initSslContext(trustManager);
+        final SSLContext sslContext = initSslContext();
         return new SSLSocketFactory(sslContext, SSLSocketFactory.ALLOW_ALL_HOSTNAME_VERIFIER);
     }
 
-    private static SSLContext initSslContext(final X509TrustManager trustManager) {
+    private static SSLContext initSslContext() {
+        final X509TrustManager trustManager = new UnsafeX509TrustManager();
         // Now put the trust manager into an SSLContext.
         // Supported: SSL, SSLv2, SSLv3, TLS, TLSv1, TLSv1.1
         final TrustManager[] trustManagers = new TrustManager[]{trustManager};
@@ -56,7 +57,7 @@ public final class SslHttpClientBuilder {
             sslContext = SSLContext.getInstance(SSL_PROTOCOL);
             sslContext.init(null, trustManagers, secureRandom);
         } catch (NoSuchAlgorithmException | KeyManagementException ex) {
-            throw new SslHttpClientException("Error in initializing SSL capable http client!!", ex);
+            throw new SslHttpClientException("Error in initSslContext", ex);
         }
         return sslContext;
     }
--- common/src/main/java/com/axiell/ehub/security/UnsafeX509TrustManager.java
+++ common/src/main/java/com/axiell/ehub/security/UnsafeX509TrustManager.java
@@ -7,13 +7,12 @@ import java.security.cert.X509Certificate;
 class UnsafeX509TrustManager implements X509TrustManager {
 
     public void checkClientTrusted(X509Certificate[] chain, String authType) throws CertificateException {
-        // Do nothing because of X and Y.
     }
 
     public void checkServerTrusted(X509Certificate[] chain, String authType) throws CertificateException {
     }
 
     public X509Certificate[] getAcceptedIssuers() {
-        return new X509Certificate[0];
+        return null;
     }
 }
