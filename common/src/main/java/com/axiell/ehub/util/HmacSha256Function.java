b7f2390274bf6af8b0b88ddc83d21ad378499e92
wsyski <wos@axiell.com>
2015-06-18 17:14:14 +0200

    ARENA-15996 - New eHub Provider - Borrowbox

--- common/src/main/java/com/axiell/ehub/util/HmacSha256Function.java
+++ common/src/main/java/com/axiell/ehub/util/HmacSha256Function.java
@@ -1,7 +1,9 @@
 package com.axiell.ehub.util;
 
 import com.axiell.ehub.InternalServerErrorException;
+import org.apache.commons.codec.DecoderException;
 import org.apache.commons.codec.binary.Base64;
+import org.apache.commons.codec.binary.Hex;
 
 import javax.crypto.Mac;
 import javax.crypto.spec.SecretKeySpec;
@@ -14,16 +16,35 @@ public class HmacSha256Function {
     private HmacSha256Function() {
     }
 
-    public static String hash(final String secretKey, final String data) {
+    public static String hash(final String secretKeyAsHexString, final String data) {
+        String algorithm = "HmacSHA256";
+        String charset = "UTF8";
+        byte[] secretKeyBytes;
         try {
-            String algorithm="HmacSHA256";
-            SecretKeySpec secretKeySpec = new SecretKeySpec(secretKey.getBytes("UTF8"), algorithm);
-            Mac mac = Mac.getInstance(algorithm);
-            mac.init(secretKeySpec);
-            return Base64.encodeBase64String(mac.doFinal(data.getBytes("UTF8")));
-        } catch (NoSuchAlgorithmException | InvalidKeyException |UnsupportedEncodingException ex) {
-            throw new InternalServerErrorException("Could not get a hash for the HmacSHA256 algorithm", ex);
+            secretKeyBytes = Hex.decodeHex(secretKeyAsHexString.toCharArray());
+        } catch (DecoderException ex) {
+            throw new InternalServerErrorException("Could not parse hex string: " + secretKeyAsHexString, ex);
         }
+        SecretKeySpec secretKeySpec = new SecretKeySpec(secretKeyBytes, algorithm);
+
+        byte[] dataBytes;
+        try {
+            dataBytes = data.getBytes(charset);
+        } catch (UnsupportedEncodingException ex) {
+            throw new InternalServerErrorException(ex.getMessage(), ex);
+        }
+        Mac mac;
+        try {
+            mac = Mac.getInstance(algorithm);
+        } catch (NoSuchAlgorithmException ex) {
+            throw new InternalServerErrorException(ex.getMessage(), ex);
+        }
+        try {
+            mac.init(secretKeySpec);
+        } catch (InvalidKeyException ex) {
+            throw new InternalServerErrorException(ex.getMessage(), ex);
+        }
+        return Base64.encodeBase64String(mac.doFinal(dataBytes));
     }
 
 }
--- common/src/test/java/com/axiell/ehub/util/HmacSha256FunctionTest.java
+++ common/src/test/java/com/axiell/ehub/util/HmacSha256FunctionTest.java
@@ -7,8 +7,8 @@ import static org.hamcrest.MatcherAssert.assertThat;
 
 public class HmacSha256FunctionTest {
     private static final String KEY = "8271ee62ad72c5e7c7b8ddbb6898835f64d2bb93";
-    private static final String DATA = "123abc";
-    private static final String EXP_HASH = "I6OiN9cdIOPzkXSOhnGXjLSRxxDWr2/2WsXJ6ItXDeo=";
+    private static final String DATA = "1001\n1200012512\n1427151942";
+    private static final String EXP_HASH = "qLD18IaZbz1P5v5KBFtaSTfKS4y5bNCYHb8X89L1N6M=";
     private String actualHash;
 
     @Test
