2a69be222a5d2a7c8a9d9cc3b6dc1fedf36e69a6
wsyski <wos@axiell.com>
2015-06-22 17:56:43 +0200

    ARENA-15996 - New eHub Provider - Borrowbox

--- provider-sdk/src/main/java/com/axiell/ehub/provider/sdk/IProviderResource.java
+++ provider-sdk/src/main/java/com/axiell/ehub/provider/sdk/IProviderResource.java
@@ -1,8 +1,5 @@
 package com.axiell.ehub.provider.sdk;
 
-import com.axiell.ehub.FieldsDTO;
-import com.axiell.ehub.security.AuthInfo;
-
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
@@ -17,12 +14,12 @@ public interface IProviderResource {
 
     @POST
     @Path("checkouts")
-    CheckoutDTO checkout(@HeaderParam("Authorization") AuthInfo authInfo, CheckoutRequestDTO checkoutRequest);
+    CheckoutDTO checkout(@HeaderParam("Authorization") Authorization authorization, CheckoutRequestDTO checkoutRequest);
 
     @GET
     @Path("checkouts/{id}")
-    CheckoutDTO getCheckout(@HeaderParam("Authorization") AuthInfo authInfo, @PathParam("id") Long ehubCheckoutId);
+    CheckoutDTO getCheckout(@HeaderParam("Authorization") Authorization authorization, @PathParam("id") Long ehubCheckoutId);
 
     @Path("records/{recordId}/formats")
-    FormatsDTO records(@PathParam("recordId") String recordId);
+    FormatsDTO records(@HeaderParam("Authorization") Authorization authorization, @PathParam("recordId") String recordId);
 }
