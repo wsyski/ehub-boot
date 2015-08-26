fc2b79dddb3b522e8ebfeceb663de2948f5e91ce
wsyski <wos@axiell.com>
2015-08-26 12:40:29 +0200

    ARENA-16102 - Fault- Change visibility in array, it gives unexpected runtime

--- remote/src/main/java/com/axiell/ehub/util/EhubExceptionUnmarshallingClientInterceptor.java
+++ remote/src/main/java/com/axiell/ehub/util/EhubExceptionUnmarshallingClientInterceptor.java
@@ -14,6 +14,7 @@ import javax.ws.rs.core.MediaType;
 /**
  * Client Exception Interceptor.
  */
+//TODO This class should be probably removed since EhubClient is ported to 3.0
 public class EhubExceptionUnmarshallingClientInterceptor implements ClientErrorInterceptor {
     private static final Logger LOGGER = LoggerFactory.getLogger(EhubExceptionUnmarshallingClientInterceptor.class);
 
@@ -22,16 +23,19 @@ public class EhubExceptionUnmarshallingClientInterceptor implements ClientErrorI
      */
     @Override
     public void handle(final ClientResponse<?> clientResponse) throws EhubRuntimeException {
-        MediaType mediaType = clientResponse.getMediaType();
-        if (isSupportedMediaClass(mediaType)) {
-            EhubError ehubError = clientResponse.getEntity(EhubError.class);
-            if (ehubError == null) {
-                ehubError = ErrorCause.INTERNAL_SERVER_ERROR.toEhubError();
+        // Prevent conflict with Arena unchecked exception
+        if (clientResponse.getStatus() != 418) {
+            MediaType mediaType = clientResponse.getMediaType();
+            if (isSupportedMediaClass(mediaType)) {
+                EhubError ehubError = clientResponse.getEntity(EhubError.class);
+                if (ehubError == null) {
+                    ehubError = ErrorCause.INTERNAL_SERVER_ERROR.toEhubError();
+                }
+                LOGGER.error(ehubError.getMessage());
+                throw new EhubRuntimeException(clientResponse.getStatus(), ehubError);
+            } else {
+                LOGGER.error(" Response:" + ToStringConverter.lineFeed() + ToStringConverter.clientResponseToString(clientResponse));
             }
-            LOGGER.error(ehubError.getMessage());
-            throw new EhubRuntimeException(clientResponse.getStatus(), ehubError);
-        } else {
-            LOGGER.error(" Response:" + ToStringConverter.lineFeed() + ToStringConverter.clientResponseToString(clientResponse));
         }
     }
 
