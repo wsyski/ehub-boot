1e0200f3e2249a3476291209e2786dbb25eaf98e
wsyski <wos@axiell.com>
2016-04-04 16:27:45 +0200

    Format code

--- local/src/main/java/com/axiell/ehub/provider/record/format/FormatDecorationEditForm.java
+++ local/src/main/java/com/axiell/ehub/provider/record/format/FormatDecorationEditForm.java
@@ -3,16 +3,16 @@ package com.axiell.ehub.provider.record.format;
 class FormatDecorationEditForm extends AbstractFormatDecorationForm<FormatDecorationMediator> {
 
     FormatDecorationEditForm(final String id, final FormatDecorationMediator mediator) {
-	super(id, mediator);
+        super(id, mediator);
     }
-    
+
     @Override
     protected boolean isNewFormatDecoration() {
         return false;
     }
-    
+
     @Override
     protected AbstractFormatDecorationSaveButton<FormatDecorationMediator> makeSaveButton(String id) {
-	return new FormatDecorationEditButton(id, formModel, mediator);
+        return new FormatDecorationEditButton(id, formModel, mediator);
     }
 }
