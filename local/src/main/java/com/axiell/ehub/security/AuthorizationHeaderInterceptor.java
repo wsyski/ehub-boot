06ffacc711d9bd67b85d6edc5a1ab71aa103e209
wsyski <wos@axiell.com>
2016-06-07 18:00:23 +0200

    ARENA-17601 - eHUB - Loan per product

--- local/src/main/java/com/axiell/ehub/security/AuthorizationHeaderInterceptor.java
+++ local/src/main/java/com/axiell/ehub/security/AuthorizationHeaderInterceptor.java
@@ -4,56 +4,41 @@
 package com.axiell.ehub.security;
 
 import com.axiell.ehub.ErrorCause;
-import org.jboss.resteasy.annotations.interception.Precedence;
-import org.jboss.resteasy.annotations.interception.ServerInterceptor;
 import org.jboss.resteasy.core.ResourceMethodInvoker;
-import org.jboss.resteasy.core.ServerResponse;
-import org.jboss.resteasy.spi.Failure;
-import org.jboss.resteasy.spi.HttpRequest;
-import org.jboss.resteasy.spi.interception.AcceptedByMethod;
-import org.jboss.resteasy.spi.interception.PreProcessInterceptor;
+import org.jboss.resteasy.core.interception.PostMatchContainerRequestContext;
 
-import javax.ws.rs.WebApplicationException;
+import javax.annotation.Priority;
+import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.container.ContainerRequestFilter;
 import javax.ws.rs.core.HttpHeaders;
 import javax.ws.rs.ext.Provider;
+import java.io.IOException;
 import java.lang.reflect.Method;
 import java.util.Arrays;
-import java.util.List;
 
-/**
- * This {@link PreProcessInterceptor} is executed before all resource methods that contains a parameter of type
- * {@link AuthInfo}. It makes sure that the request contains an <code>Authorization</code> header, if it doesn't an
- * {@link UnauthorizedException} is thrown.
- */
 @Provider
-@Precedence(value = "SECURITY")
-@ServerInterceptor
-final class AuthorizationHeaderInterceptor implements PreProcessInterceptor, AcceptedByMethod {
+@Priority(Integer.MIN_VALUE)
+public final class AuthorizationHeaderInterceptor implements ContainerRequestFilter {
 
-    /**
-     * @see org.jboss.resteasy.spi.interception.AcceptedByMethod#accept(java.lang.Class, java.lang.reflect.Method)
-     */
     @Override
-    public boolean accept(@SuppressWarnings("rawtypes") Class c, Method method) {
-        final Class<?>[] parameterTypes = method.getParameterTypes();
-        final List<Class<?>> parameterTypeList = Arrays.asList(parameterTypes);
-        return parameterTypeList.contains(AuthInfo.class);
+    public void filter(final ContainerRequestContext requestContext) throws IOException {
+        if (requestContext instanceof PostMatchContainerRequestContext) {
+            ResourceMethodInvoker resourceMethodInvoker = ((PostMatchContainerRequestContext) requestContext).getResourceMethod();
+            Method method = resourceMethodInvoker.getMethod();
+            if (methodParametersContainAuthInfo(method)) {
+                final String authorization = requestContext.getHeaderString(HttpHeaders.AUTHORIZATION);
+                if (authorization == null || authorization.isEmpty()) {
+                    throw new UnauthorizedException(
+                            "The method '" + method.getName() + "' in the resource '" + resourceMethodInvoker.getResourceClass()
+                                    + "'expects an 'AuthInfo', but the request does not contain an Authorization header",
+                            ErrorCause.MISSING_AUTHORIZATION_HEADER);
+                }
+            }
+        }
     }
 
-    /**
-     * @see org.jboss.resteasy.spi.interception.PreProcessInterceptor#preProcess(org.jboss.resteasy.spi.HttpRequest,
-     * org.jboss.resteasy.core.ResourceMethod)
-     */
-    @Override
-    public ServerResponse preProcess(final HttpRequest httpRequest, final ResourceMethodInvoker resourceMethodInvoker) throws Failure, WebApplicationException {
-        final HttpHeaders headers = httpRequest.getHttpHeaders();
-        final List<String> authorizationHeaders = headers.getRequestHeader("Authorization");
-
-        if (authorizationHeaders == null || authorizationHeaders.isEmpty()) {
-            throw new UnauthorizedException("The method '" + resourceMethodInvoker.getMethod().getName() + "' in the resource '" + resourceMethodInvoker.getResourceClass()
-                    + "'expects an 'AuthInfo', but the request does not contain an Authorization header", ErrorCause.MISSING_AUTHORIZATION_HEADER);
-        }
-
-        return null;
+    private boolean methodParametersContainAuthInfo(final Method method) {
+        Class<?>[] parameterTypes = method.getParameterTypes();
+        return Arrays.stream(parameterTypes).anyMatch(clazz -> clazz.equals(AuthInfo.class));
     }
 }
