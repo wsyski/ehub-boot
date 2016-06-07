fedce07618f354ee929a8a2065eed7f1dc787485
wsyski <wos@axiell.com>
2016-06-07 12:23:59 +0200

    ARENA-17601 - eHUB - Loan per product

---
+++ integration-tests/tests/remote/src/test/java/com/axiell/ehub/it/RemoteLoanITFixture.java
@@ -0,0 +1,122 @@
+package com.axiell.ehub.it;
+
+import com.axiell.ehub.EhubException;
+import com.axiell.ehub.ErrorCause;
+import com.axiell.ehub.ErrorCauseArgument;
+import com.axiell.ehub.Fields;
+import com.axiell.ehub.checkout.*;
+import com.axiell.ehub.test.TestDataConstants;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Date;
+
+import static com.axiell.ehub.checkout.ContentLinkMatcher.matchesExpectedContentLink;
+import static com.axiell.ehub.checkout.SupplementLinkMatcher.matchesExpectedSupplementLink;
+import static com.github.tomakehurst.wiremock.client.WireMock.*;
+import static org.junit.Assert.assertThat;
+
+public abstract class RemoteLoanITFixture extends RemoteITFixture {
+    protected Fields fields;
+    protected String lmsLoanId;
+    protected Long readyLoanId;
+
+    @Before
+    public void initFields() {
+        fields = new Fields();
+        fields.addValue("lmsRecordId", TestDataConstants.LMS_RECORD_ID);
+        fields.addValue("contentProviderAlias", CONTENT_PROVIDER_ALIAS);
+        fields.addValue("contentProviderRecordId", TestDataConstants.TEST_EP_RECORD_0_ID);
+    }
+
+    @Test
+    public final void checkoutWithCheckoutTestErrorResponse() throws EhubException {
+        givenExpectedEhubException(ErrorCause.LMS_ERROR.toEhubError(new ErrorCauseArgument(ErrorCauseArgument.Type.LMS_STATUS, "blockedBorrCard"),
+                new ErrorCauseArgument(ErrorCauseArgument.Type.EHUB_CONSUMER_ID, String.valueOf(testData.getEhubConsumerId()))));
+        givenContentProviderFormatId(TestDataConstants.TEST_EP_FORMAT_1_ID);
+        givenPalmaLoansWsdl();
+        givenCheckoutTestErrorResponse();
+        Checkout checkout = whenCheckout();
+    }
+
+    protected void givenContentProviderGetCheckoutResponse() {
+        stubFor(get(urlEqualTo("/ep/api/v1/checkouts/" + TestDataConstants.CONTENT_PROVIDER_LOAN_ID))
+                .willReturn(
+                        aResponse().withBodyFile(getResponseFilePrefix() + "CheckoutResponse_activeLoan.json").withHeader("Content-Type", "application/json")
+                                .withStatus(200)));
+    }
+
+    protected void givenContentProviderCheckoutResponse() {
+        stubFor(post(urlEqualTo("/ep/api/v1/checkouts"))
+                .willReturn(aResponse().withBodyFile(getResponseFilePrefix() + "CheckoutResponse_newLoan.json").withHeader("Content-Type", "application/json")
+                        .withStatus(201)));
+    }
+
+    private String getResponseFilePrefix() {
+        return isLoanPerProduct() ? "lpp" : "lpf";
+    }
+
+    protected void thenValidCheckout(final Checkout checkout, final String contentProviderFormatId) {
+        Assert.assertNotNull(checkout);
+        thenValidCheckoutMetadata(checkout.metadata());
+        thenValidSupplementLinks(checkout.supplementLinks(), contentProviderFormatId);
+        thenValidContentLinks(checkout.contentLinks(), contentProviderFormatId);
+    }
+
+    protected void thenValidCheckoutMetadata(final CheckoutMetadata checkoutMetadata) {
+        Assert.assertNotNull(checkoutMetadata);
+        Date expirationDate = checkoutMetadata.expirationDate();
+        Assert.assertNotNull(expirationDate);
+        String lmsLoanId = checkoutMetadata.lmsLoanId();
+        Assert.assertNotNull(lmsLoanId);
+        Long id = checkoutMetadata.id();
+        Assert.assertNotNull(id);
+    }
+
+    private void thenValidContentLinks(final ContentLinks contentLinks, final String contentProviderFormatId) {
+        Assert.assertNotNull(contentLinks);
+        Assert.assertEquals(2, contentLinks.getContentLinks().size());
+        assertThat(contentLinks.getContentLinks().get(0),
+                matchesExpectedContentLink(new ContentLink("http:/localhost:16521/ep/api/v1/records/recordId_0/" + contentProviderFormatId + "/content_0")));
+        assertThat(contentLinks.getContentLinks().get(1),
+                matchesExpectedContentLink(new ContentLink("http:/localhost:16521/ep/api/v1/records/recordId_0/" + contentProviderFormatId + "/content_1")));
+    }
+
+    private void thenValidSupplementLinks(final SupplementLinks supplementLinks, final String contentProviderFormatId) {
+        Assert.assertNotNull(supplementLinks);
+        Assert.assertEquals(2, supplementLinks.getSupplementLinks().size());
+        assertThat(supplementLinks.getSupplementLinks().get(0),
+                matchesExpectedSupplementLink(
+                        new SupplementLink("supplement_0", "http:/localhost:16521/ep/api/v1/records/recordId_0/" + contentProviderFormatId + "/supplement_0")));
+        assertThat(supplementLinks.getSupplementLinks().get(1),
+                matchesExpectedSupplementLink(
+                        new SupplementLink("supplement_1", "http:/localhost:16521/ep/api/v1/records/recordId_0/" + contentProviderFormatId + "/supplement_1")));
+    }
+
+    protected void givenLmsLoanId() {
+        lmsLoanId = TestDataConstants.LMS_LOAN_ID;
+    }
+
+    protected void givenReadyLoanId() {
+        readyLoanId = testData.getEhubLoanId();
+    }
+
+    protected void givenContentProviderFormatId(final String contentProviderFormatId) {
+        fields.addValue("contentProviderFormatId", contentProviderFormatId);
+    }
+
+    protected Checkout whenCheckout() throws EhubException {
+        Assert.assertNotNull(fields.getRequiredValue("contentProviderFormatId"));
+        return underTest.checkout(authInfo, fields, LANGUAGE);
+    }
+
+    protected CheckoutMetadata whenFindCheckoutMetadataByLmsLoandId() throws EhubException {
+        return underTest.findCheckoutByLmsLoanId(authInfo, lmsLoanId, LANGUAGE);
+    }
+
+    protected Checkout whenGetCheckoutByLoanId() throws EhubException {
+        return underTest.getCheckout(authInfo, readyLoanId, LANGUAGE);
+    }
+
+}
