218a136f593d42027ab2ef7a4554a536c221e2b6
wsyski <wos@axiell.com>
2016-06-13 15:20:47 +0200

    ARENA-17601 - eHUB - Loan per product

---
+++ local/src/test/java/com/axiell/ehub/provider/ep/lpf/AbstractLpfEpIT.java
@@ -0,0 +1,49 @@
+package com.axiell.ehub.provider.ep.lpf;
+
+import com.axiell.ehub.provider.ep.AbstractEpIT;
+import com.axiell.ehub.provider.ep.EpUserIdValue;
+import com.axiell.ehub.provider.ep.FormatMetadataDTO;
+import org.junit.Test;
+
+import static junit.framework.Assert.assertNotNull;
+
+public abstract class AbstractLpfEpIT extends AbstractEpIT<LpfEpFacade, LpfCheckoutDTO> {
+
+    @Test
+    public void checkout()  {
+        givenLibraryCardInPatron();
+        givenConfigurationProperties(EpUserIdValue.LIBRARY_CARD);
+        givenContentProvider();
+        givenEhubConsumer();
+        whenCheckout();
+        thenCheckoutHasTransactionId();
+        thenCheckoutHasExpirationDate();
+        thenCheckoutHasExpectedDownloadUrls();
+        thenPatronHasCheckout();
+    }
+
+    private void whenCheckout() {
+        checkout = underTest.checkout(contentProviderConsumer, patron, getRecordId(), getFormatIds().get(0));
+    }
+
+    private void thenPatronHasCheckout() {
+        checkout = underTest.getCheckout(contentProviderConsumer, patron, checkout.getId());
+    }
+
+    private void thenCheckoutHasExpectedDownloadUrls() {
+        final FormatMetadataDTO formatMetadata = checkout.getFormatMetadata();
+        assertNotNull(formatMetadata);
+        assertNotNull(formatMetadata.getContentLinks());
+    }
+
+    @Override
+    protected LpfEpFacade createEpFacade() {
+        return new LpfEpFacade();
+    }
+
+    @Override
+    protected boolean isLoanPerProduct() {
+        return false;
+    }
+
+}
---
+++ local/src/test/java/com/axiell/ehub/provider/ep/lpp/ulverscroft/UlverscroftIT.java
@@ -0,0 +1,41 @@
+package com.axiell.ehub.provider.ep.lpp.ulverscroft;
+
+import com.axiell.ehub.provider.ep.lpp.AbstractLppEpIT;
+import com.google.common.collect.Lists;
+
+import java.util.List;
+
+public class UlverscroftIT extends AbstractLppEpIT {
+    private static final String API_BASE_URL_VALUE = "https://xyzaeh.ulverscroftdigital.com";
+    private static final String EP_SITE_ID = "1111";
+    private static final String EP_SECRET_KEY = "c2VjcmV0S2V5";
+    private static final String RECORD_ID = "9781407941011";
+    private static final String FORMAT_ID_0 = "mp3";
+    private static final String FORMAT_ID_1 = "m4b";
+
+    @Override
+    protected List<String> getFormatIds() {
+         return Lists.newArrayList(FORMAT_ID_0,FORMAT_ID_1);
+    }
+
+    @Override
+    protected String getApiBaseUri() {
+        return API_BASE_URL_VALUE;
+    }
+
+    @Override
+    protected String getSiteId() {
+        return EP_SITE_ID;
+    }
+
+    @Override
+    protected String getSecretKey() {
+        return EP_SECRET_KEY;
+    }
+
+    @Override
+    protected String getRecordId() {
+        return RECORD_ID;
+    }
+
+}
