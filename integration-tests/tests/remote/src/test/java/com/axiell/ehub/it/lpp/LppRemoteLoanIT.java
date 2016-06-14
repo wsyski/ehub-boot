fef8ac64fdfc4e7f47ad2cda599fca678f19f1be
wsyski <wos@axiell.com>
2016-06-14 11:54:37 +0200

    ARENA-17601 - eHUB - Loan per product

--- integration-tests/tests/remote/src/test/java/com/axiell/ehub/it/lpp/LppRemoteLoanIT.java
+++ integration-tests/tests/remote/src/test/java/com/axiell/ehub/it/lpp/LppRemoteLoanIT.java
@@ -1,6 +1,9 @@
 package com.axiell.ehub.it.lpp;
 
 import com.axiell.ehub.EhubException;
+import com.axiell.ehub.ErrorCause;
+import com.axiell.ehub.ErrorCauseArgument;
+import com.axiell.ehub.ErrorCauseArgumentValue;
 import com.axiell.ehub.checkout.Checkout;
 import com.axiell.ehub.checkout.CheckoutMetadata;
 import com.axiell.ehub.it.RemoteLoanITFixture;
@@ -8,6 +11,18 @@ import com.axiell.ehub.test.TestDataConstants;
 import org.junit.Test;
 
 public class LppRemoteLoanIT extends RemoteLoanITFixture {
+
+    @Test
+    public final void checkoutWithContentProviderError() throws EhubException {
+        givenExpectedEhubException(ErrorCause.CONTENT_PROVIDER_ERROR.toEhubError(new ErrorCauseArgument(ErrorCauseArgument.Type.CONTENT_PROVIDER_NAME, TestDataConstants.CONTENT_PROVIDER_TEST_EP),
+                new ErrorCauseArgument(ErrorCauseArgument.Type.CONTENT_PROVIDER_STATUS, ErrorCauseArgumentValue.Type.ALREADY_ON_LOAN)));
+        givenContentProviderFormatId(TestDataConstants.TEST_EP_FORMAT_0_ID);
+        givenPalmaLoansWsdl();
+        givenPalmaCheckoutTestNewLoanResponse();
+        givenPalmaCheckoutResponse();
+        givenContentProviderCheckoutErrorResponse(ErrorCauseArgumentValue.Type.ALREADY_ON_LOAN);
+        Checkout checkout = whenCheckout();
+    }
 
     @Test
     public final void checkoutWithExistingContentProviderLoan() throws EhubException {
