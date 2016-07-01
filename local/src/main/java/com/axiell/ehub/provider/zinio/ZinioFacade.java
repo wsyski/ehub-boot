4705638f1a48c5de419a9ddb8f4cc0294622cad8
wsyski <wos@axiell.com>
2016-07-01 18:12:52 +0200

    ARENA-16888 - Implement - new e-media provider Zinio

--- local/src/main/java/com/axiell/ehub/provider/zinio/ZinioFacade.java
+++ local/src/main/java/com/axiell/ehub/provider/zinio/ZinioFacade.java
@@ -4,8 +4,6 @@ import com.axiell.ehub.InternalServerErrorException;
 import com.axiell.ehub.consumer.ContentProviderConsumer;
 import com.axiell.ehub.patron.Patron;
 import com.axiell.ehub.util.EhubUrlCodec;
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.commons.lang3.RandomStringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
