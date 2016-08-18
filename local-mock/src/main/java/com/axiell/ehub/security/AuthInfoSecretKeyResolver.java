b7b4e4c75cbf8c50cd7aa0207ac052046d84159b
wsyski <wos@axiell.com>
2016-08-18 15:18:33 +0200

    Added mock ehub project

--- local-mock/src/main/java/com/axiell/ehub/security/AuthInfoSecretKeyResolver.java
+++ local-mock/src/main/java/com/axiell/ehub/security/AuthInfoSecretKeyResolver.java
@@ -1,10 +1,14 @@
 package com.axiell.ehub.security;
 
 public class AuthInfoSecretKeyResolver implements IAuthInfoSecretKeyResolver {
-    private static final String SECRET_KEY = "c2VjcmV0S2V5";
+    private String secretKey;
+
+    public AuthInfoSecretKeyResolver(final String secretKey) {
+        this.secretKey = secretKey;
+    }
 
     @Override
     public String getSecretKey(long ehubConsumerId) {
-        return SECRET_KEY;
+        return secretKey;
     }
 }
