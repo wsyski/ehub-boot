1d2e50b77905dc4832da074f64f4272054a7ccad
wsyski <wos@axiell.com>
2016-08-18 16:43:23 +0200

    Added mock ehub project

--- local-mock/src/main/java/com/axiell/ehub/provider/record/issue/IssueBusinessController.java
+++ local-mock/src/main/java/com/axiell/ehub/provider/record/issue/IssueBusinessController.java
@@ -1,14 +1,15 @@
 package com.axiell.ehub.provider.record.issue;
 
-import com.axiell.ehub.provider.record.issue.IIssueBusinessController;
-import com.axiell.ehub.provider.record.issue.Issue;
 import com.axiell.ehub.security.AuthInfo;
+import com.axiell.ehub.util.EhubMessageUtility;
 
 import java.util.List;
 
 public class IssueBusinessController implements IIssueBusinessController {
+    private EhubMessageUtility ehubMessageUtility = new EhubMessageUtility();
+
     @Override
     public List<Issue> getIssues(final AuthInfo authInfo, final String contentProviderName, final String contentProviderRecordId, final String language) {
-        return null;
+        return ehubMessageUtility.getEhubMessage(List.class,"records",contentProviderName,contentProviderRecordId);
     }
 }
