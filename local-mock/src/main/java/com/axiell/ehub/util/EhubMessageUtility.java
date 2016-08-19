89cc67b13382d978d9169d24b62b119a5ca0c4ab
wsyski <wos@axiell.com>
2016-08-19 11:04:55 +0200

    Added mock ehub project

--- local-mock/src/main/java/com/axiell/ehub/util/EhubMessageUtility.java
+++ local-mock/src/main/java/com/axiell/ehub/util/EhubMessageUtility.java
@@ -1,9 +1,12 @@
 package com.axiell.ehub.util;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 
+import javax.servlet.ServletContext;
 import java.io.File;
 import java.io.IOException;
 import java.nio.charset.Charset;
@@ -12,37 +15,25 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class EhubMessageUtility {
+    @Autowired
+    private ServletContext servletContext;
+
     private static final Logger LOGGER = LoggerFactory.getLogger(EhubMessageUtility.class);
-    private static String ehubResponsesDir;
-    private static int ehubMaxTimeout;
     private static final String FILE_SUFFIX = ".json";
     private static final String KEY_SEPARATOR = "-";
 
-    /**
-     * Private constructor that prevent direct instantiation.
-     */
-    public EhubMessageUtility() {
-        configureEhubResponseDir();
-    }
-
-    private void configureEhubResponseDir() {
+    private String getResponseDir() {
         final String ehubDir = System.getProperty("ehubDir");
-        if (ehubDir == null || ehubDir.equals("")) {
-            ehubResponsesDir = System.getProperty("catalina.base") + File.separator + "webapp"+ File.separator + "config"+ File.separator + "ehub";
+        String ehubResponseDir;
+        if (StringUtils.isBlank(ehubDir)) {
+            ehubResponseDir = servletContext.getRealPath(servletContext.getContextPath()) + File.separator + "WEB-INF" + File.separator + "config" + File.separator + "default";
         } else {
-            ehubResponsesDir = ehubDir;
+            ehubResponseDir = ehubDir;
         }
-
-        final String ehubTimeout = System.getProperty("ehubMaxTimeout");
-        if (ehubTimeout == null || ehubTimeout.equals("")) {
-            ehubMaxTimeout = 0;
-        } else {
-            ehubMaxTimeout = Integer.parseInt(ehubTimeout);
-        }
-        LOGGER.info("Using max timeout " + ehubMaxTimeout);
+        return ehubResponseDir;
     }
 
-    public <T> T getEhubMessage(Class<T> clazz, String... fileNamePart)  {
+    public <T> T getEhubMessage(Class<T> clazz, String... fileNamePart) {
         final List<String> fileNames = getPossibleFileNames((String[]) fileNamePart);
         final File file = getEhubMessageFile(fileNames);
         try {
@@ -50,9 +41,8 @@ public class EhubMessageUtility {
             String json = new String(encoded, Charset.forName("UTF-8"));
             ObjectMapper mapper = new ObjectMapper();
             return mapper.readValue(json, clazz);
-        }
-        catch(IOException ex) {
-            LOGGER.error(ex.getMessage(),ex);
+        } catch (IOException ex) {
+            LOGGER.error(ex.getMessage(), ex);
             return null;
         }
     }
@@ -78,10 +68,11 @@ public class EhubMessageUtility {
         return fileNames;
     }
 
-    private static File getEhubMessageFile(final List<String> fileNames) {
+    private File getEhubMessageFile(final List<String> fileNames) {
+        String responseDir = getResponseDir();
         for (String fileName : fileNames) {
             LOGGER.info("Look for file: " + fileName);
-            final File file = new File(ehubResponsesDir + File.separator + fileName + FILE_SUFFIX);
+            final File file = new File(responseDir + File.separator + fileName + FILE_SUFFIX);
             if (file.exists() && file.isFile()) {
                 LOGGER.info("Found file: " + fileName);
                 return file;
