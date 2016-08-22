f8da7b7e740c20055b72c57b915a9e230004b4ee
wsyski <wos@axiell.com>
2016-08-22 11:27:34 +0200

    Added mock ehub project

--- local-mock/src/main/java/com/axiell/ehub/loan/LoanBusinessController.java
+++ local-mock/src/main/java/com/axiell/ehub/loan/LoanBusinessController.java
@@ -1,16 +1,23 @@
 package com.axiell.ehub.loan;
 
-import com.axiell.ehub.Fields;
+import com.axiell.ehub.*;
 import com.axiell.ehub.checkout.Checkout;
 import com.axiell.ehub.checkout.CheckoutDTO;
 import com.axiell.ehub.checkout.CheckoutMetadataDTO;
 import com.axiell.ehub.checkout.CheckoutsSearchResult;
+import com.axiell.ehub.consumer.ContentProviderConsumer;
+import com.axiell.ehub.error.IEhubExceptionFactory;
+import com.axiell.ehub.provider.ContentProvider;
 import com.axiell.ehub.search.SearchResultDTO;
 import com.axiell.ehub.security.AuthInfo;
 import com.axiell.ehub.util.EhubMessageUtility;
 import org.springframework.beans.factory.annotation.Autowired;
 
+
 public class LoanBusinessController implements ILoanBusinessController {
+    @Autowired
+    private IEhubExceptionFactory ehubExceptionFactory;
+
     @Autowired
     private EhubMessageUtility ehubMessageUtility;
 
@@ -18,7 +25,7 @@ public class LoanBusinessController implements ILoanBusinessController {
     public CheckoutsSearchResult search(final AuthInfo authInfo, final String lmsLoanId, final String language) {
         SearchResultDTO<CheckoutMetadataDTO> searchResultDTO = ehubMessageUtility.getEhubMessage(SearchResultDTO.class, "search",
                 String.valueOf(lmsLoanId), authInfo.getPatron().getLibraryCard());
-        return new CheckoutsSearchResult(searchResultDTO);
+        return new CheckoutsSearchResult(searchResultDTO==null ? new SearchResultDTO<>() : searchResultDTO);
     }
 
     @Override
@@ -30,6 +37,13 @@ public class LoanBusinessController implements ILoanBusinessController {
         String contentProviderFormatId = fields.getRequiredValue("contentProviderFormatId");
         CheckoutDTO checkoutDTO = ehubMessageUtility.getEhubMessage(CheckoutDTO.class, "checkout", contentProviderAlias, contentProviderRecordId,
                 contentProviderIssueId, contentProviderFormatId, authInfo.getPatron().getLibraryCard());
+        if (checkoutDTO == null) {
+            ContentProviderConsumer contentProviderConsumer=new ContentProviderConsumer();
+            ContentProvider contentProvider=new ContentProvider();
+            contentProvider.setName(contentProviderAlias);
+            contentProviderConsumer.setContentProvider(contentProvider);
+            throw ehubExceptionFactory.createInternalServerErrorExceptionWithContentProviderNameAndStatus(contentProviderConsumer, ErrorCauseArgumentType.CREATE_LOAN_FAILED, language);
+        }
         return new Checkout(checkoutDTO);
     }
 
@@ -37,6 +51,10 @@ public class LoanBusinessController implements ILoanBusinessController {
     public Checkout getCheckout(final AuthInfo authInfo, final long readyLoanId, final String language) {
         CheckoutDTO checkoutDTO = ehubMessageUtility.getEhubMessage(CheckoutDTO.class, "checkout", String.valueOf(readyLoanId),
                 authInfo.getPatron().getLibraryCard());
+        if (checkoutDTO == null) {
+            final ErrorCauseArgument argument = new ErrorCauseArgument(ErrorCauseArgument.Type.READY_LOAN_ID, readyLoanId);
+            throw new NotFoundException(ErrorCause.LOAN_BY_ID_NOT_FOUND, argument);
+        }
         return new Checkout(checkoutDTO);
     }
 }
