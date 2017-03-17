0078ae12569f303fabc4193a0991ed77b0b02506
wsyski <wos@axiell.com>
2017-03-17 13:05:54 +0100

    ARENA-18106 - Translate Askews error 0 into something more meaningful

---
+++ local-core/src/main/java/com/axiell/ehub/security/AuthorizationHeaderParts.java
@@ -0,0 +1,19 @@
+package com.axiell.ehub.security;
+
+public class AuthorizationHeaderParts {
+    private String scheme;
+    private String parameters;
+
+    public AuthorizationHeaderParts(final String scheme, final String parameters) {
+        this.scheme = scheme;
+        this.parameters = parameters;
+    }
+
+    public String getScheme() {
+        return scheme;
+    }
+
+    public String getParameters() {
+        return parameters;
+    }
+}
