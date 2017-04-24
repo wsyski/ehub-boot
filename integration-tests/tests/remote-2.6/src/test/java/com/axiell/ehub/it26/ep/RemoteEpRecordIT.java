dddb89a30c8865077e8ac90b9d7884be56b01b1d
wsyski <wos@axiell.com>
2017-04-24 20:54:16 +0200

    ARENA-18030 - Ehub should accept JSON web token produced by arena

---
+++ integration-tests/tests/remote-2.6/src/test/java/com/axiell/ehub/it26/ep/RemoteEpRecordIT.java
@@ -0,0 +1,38 @@
+package com.axiell.ehub.it26.ep;
+
+import com.axiell.ehub.it26.RemoteRecordITFixture;
+import com.axiell.ehub.test.TestDataConstants;
+
+import javax.servlet.http.HttpServletResponse;
+
+import static com.github.tomakehurst.wiremock.client.WireMock.*;
+
+public class RemoteEpRecordIT extends RemoteRecordITFixture {
+
+    @Override
+    protected void givenContentProviderGetRecordResponse() {
+        stubFor(get(urlEqualTo("/ep/api/v1/records/" + TestDataConstants.RECORD_ID_0))
+                .willReturn(aResponse().withBodyFile(getContentProviderName() + "/getRecordResponse.json").withHeader("Content-Type", "application/json")
+                        .withStatus(HttpServletResponse.SC_OK)));
+    }
+
+    @Override
+    protected String getContentProviderName() {
+        return TestDataConstants.CONTENT_PROVIDER_TEST_EP;
+    }
+
+    @Override
+    protected boolean isLoanPerProduct() {
+        return false;
+    }
+
+    @Override
+    protected int getExpectedIssueCount() {
+        return 1;
+    }
+
+    @Override
+    protected int getExpectedFormatCount() {
+        return 3;
+    }
+}
