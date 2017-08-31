dcf6add453d021fedcd9203464ad78e27ed3459c
wsyski <wos@axiell.com>
2017-08-31 14:44:54 +0200

    ARENA-18218 - Replace custom Authorization Header in the Ehub Provider Interface with JWT

--- authinfo/src/main/java/com/axiell/authinfo/InvalidAuthorizationHeaderSignatureRuntimeException.java
+++ authinfo/src/main/java/com/axiell/authinfo/InvalidAuthorizationHeaderSignatureRuntimeException.java
@@ -2,6 +2,10 @@ package com.axiell.authinfo;
 
 public class InvalidAuthorizationHeaderSignatureRuntimeException extends AuthInfoRuntimeException {
 
+    public InvalidAuthorizationHeaderSignatureRuntimeException(Throwable ex) {
+        super("Invalid Authorization Header Signature", ex);
+    }
+
     public InvalidAuthorizationHeaderSignatureRuntimeException() {
         super("Invalid Authorization Header Signature");
     }
