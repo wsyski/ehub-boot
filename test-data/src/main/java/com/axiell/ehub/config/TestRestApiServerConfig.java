f1cda26175148c46a35d079d55b55e81a721ba5b
wos <wos@axiell.com>
2026-02-10 16:53:49 +0100

    Converting to Spring Boot

---
+++ test-data/src/main/java/com/axiell/ehub/config/TestRestApiServerConfig.java
@@ -0,0 +1,41 @@
+package com.axiell.ehub.config;
+
+import com.axiell.ehub.controller.internal.TestRootResource;
+import com.axiell.ehub.controller.provider.converter.AuthInfoParamConverterProvider;
+import com.axiell.ehub.controller.provider.json.JsonProvider;
+import org.apache.cxf.Bus;
+import org.apache.cxf.endpoint.Server;
+import org.apache.cxf.ext.logging.LoggingFeature;
+import org.apache.cxf.feature.Feature;
+import org.apache.cxf.jaxrs.JAXRSServerFactoryBean;
+import org.apache.cxf.metrics.MetricsFeature;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+@Configuration
+public class TestRestApiServerConfig {
+    @Autowired
+    private Bus bus;
+
+    @Bean
+    public Server testRestApiServer(
+            final TestRootResource testRootResource,
+            final JsonProvider jsonProvider,
+            final MetricsFeature metricsFeature,
+            final LoggingFeature loggingFeature) {
+        final JAXRSServerFactoryBean endpoint = new JAXRSServerFactoryBean();
+        endpoint.setBus(bus);
+        endpoint.setAddress("/test");
+        endpoint.setServiceBean(testRootResource);
+        final List<?> providers = Collections.singletonList(jsonProvider);
+        final List<Feature> features = List.of(loggingFeature, metricsFeature);
+        endpoint.setProviders(providers);
+        endpoint.setFeatures(features);
+        return endpoint.create();
+    }
+}
